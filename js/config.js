5e22514ae6c7844e668077a518b23107efe7141b
KhoaLe992 <128986222+KhoaLe992@users.noreply.github.com>
2023-04-23 16:26:21 -0500

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,7 +1,7 @@
 window._config = {
     cognito: {
-        userPoolId:         'us-east-2_elKDqNA2V',         // e.g. us-east-2_uXboG5pAb
-        userPoolClientId:   '4fkva3v196gn0e4586g9fqjbtc',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId:         '',         // e.g. us-east-2_uXboG5pAb
+        userPoolClientId:   '',         // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region:             'us-east-2'          // e.g. us-east-2
     },
     api: {
